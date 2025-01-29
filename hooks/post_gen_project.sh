9d7a55ef727756aae4e89fa9b87fbfda242d4e84
jun-harashima <j.harashima@gmail.com>
2025-01-29 12:59:23 +0900

    use a project name as a repository name

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,5 +4,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin git@github.com:{{ cookiecutter.organization_name }}/{{ cookiecutter.project_slug }}.git
+git remote add origin git@github.com:{{ cookiecutter.organization_name }}/{{ cookiecutter.project_name }}.git
 git push -u origin main
