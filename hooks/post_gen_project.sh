db2d8030db8535dfa1146e3d9c790c0c91ebef62
jun-harashima <j.harashima@gmail.com>
2025-02-01 09:58:36 +0900

    add git_push to cookiecutter.json

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,4 +5,9 @@ git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:{{ cookiecutter.organization_name }}/{{ cookiecutter.project_name }}.git
-git push -u origin main
+
+if [ "{{ cookiecutter.git_push }}" == "True" ]; then
+  git push -u origin main
+else
+  echo "Push to GitHub is skipped."
+fi
